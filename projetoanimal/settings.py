59cb3df5ae88a620a6d1879f55da86b28d4f0c7c
Lorenzo MR <lorenzo.mr.rm@gmail.com>
2022-11-15 13:34:18 -0300

    novo allowed host

--- projetoanimal/settings.py
+++ projetoanimal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')0ex2r9^actjv%a8fo(=#8^@a$k+0ledyunrxbn$%xf&bq@g!p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['projetomiau.herokuapp.com']
 
 
 # Application definition
