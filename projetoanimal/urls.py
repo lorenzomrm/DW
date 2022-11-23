f323f2dde0047816d1dee6f67106009dca927e24
Lorenzo MR <lorenzo.mr.rm@gmail.com>
2022-11-23 11:47:27 -0300

    Página 'adoção' e rotas

--- projetoanimal/urls.py
+++ projetoanimal/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('telainicial.urls'))
+    path('', include('telainicial.urls')),
+    path('index', include('telainicial.urls')),
+    path('adocao', include('telainicial.urls')),
 ]
--- telainicial/urls.py
+++ telainicial/urls.py
@@ -1,5 +1,5 @@
 
-from telainicial.views import index
+from telainicial.views import index, adocao
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index),
+    path('adocao/', adocao)
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
